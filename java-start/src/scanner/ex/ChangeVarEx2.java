a99cdcbff7fe893cb8b026b8b2c492d708f906c7
Whatwhy <hdk8572@gmail.com>
2023-12-14 23:27:56 +0900

    ScannerWhile 연습!

---
+++ java-start/src/scanner/ex/ChangeVarEx2.java
@@ -0,0 +1,40 @@
+package scanner.ex;
+
+import java.util.Scanner;
+
+public class ChangeVarEx2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int a = scanner.nextInt();
+
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int b = scanner.nextInt();
+            int temp;
+
+            if (a == 0 && b == 0) {
+                System.out.println("프로그램 종료 했습니다.");
+                break;
+            }
+            if (a > b) {
+                temp = a;
+                a = b;
+                b = temp;
+                System.out.println("첫 번째 값은 " + a + ", 두 번째 값은 " + b);
+            } else {
+                System.out.println("첫 번째 값은 " + a + ", 두 번째 값은 " + b);
+            }
+
+            for (int i = a; i <= b; i++) {
+                System.out.print(i);
+                if(i != b) {
+                    System.out.print(",");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
