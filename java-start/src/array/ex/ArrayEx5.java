c7adf989b819cfa634ece9358dc99fc0453cf34e
Whatwhy <hdk8572@gmail.com>
2023-12-15 15:30:46 +0900

    2차원 배열 연습 (재풀이)

---
+++ java-start/src/array/ex/ArrayEx5.java
@@ -0,0 +1,27 @@
+package array.ex;
+
+import java.util.Scanner;
+
+public class ArrayEx5 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("입력 받을 숫자의 개수를 입력하세요: ");
+        int count = input.nextInt();
+
+        int[] numbers = new int[count];
+        int sum = 0;
+        double average;
+
+        System.out.println(count + "개의 정수를 입력해주세요.");
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = input.nextInt();
+            sum += numbers[i];
+        }
+
+        average = (double) sum / count;
+
+        System.out.println("sum = " + sum);
+        System.out.println("average = " + average);
+    }
+}
