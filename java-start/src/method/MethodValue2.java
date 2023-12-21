cf615d4d80aeda53e32ecd701a00ba07f24735e0
Whatwhy <hdk8572@gmail.com>
2023-12-21 13:15:38 +0900

    형 변환 복습

--- java-start/src/method/MethodValue2.java
+++ java-start/src/method/MethodValue2.java
@@ -2,16 +2,15 @@ package method;
 
 public class MethodValue2 {
     public static void main(String[] args) {
-        int number = 5;
-        System.out.println("1. changeNumber 호출 전, number: " + number);
-        changeNumber(number);
-        System.out.println("4. changeNumber 호출 후, number: " + number);
+        int num1 = 5;
+        System.out.println("1. changeNumber 호출 전, num1: " + num1);
+        changeNumber(num1);
+        System.out.println("4. changeNumber 호출 후, num1: " + num1);
     }
 
-    public static void changeNumber(int number) {
-        System.out.println("2. changeNumber 변경 전, number: " + number);
-        number = number * 2;
-        System.out.println("3. changeNumber 변경 후, number: " + number);
+    public static int changeNumber(int num2) {
+        num2 = num2 * 2;
+        return num2;
     }
 
 
---
+++ java-start/src/method/Overloading3.java
@@ -0,0 +1,13 @@
+package method;
+
+public class Overloading3 {
+    public static void main(String[] args) {
+        System.out.println("1: " + add(1, 2));
+        System.out.println("2: " + add(1.2, 1.5));
+    }
+
+    public static double add(double a, double b) {
+        System.out.println("double a, double b");
+        return a + b;
+    }
+}
