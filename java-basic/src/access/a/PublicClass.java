b7736837242d7837510c770a61e20650a81d3cbc
Whatwhy <hdk8572@gmail.com>
2024-01-12 14:33:41 +0900

    접근제어자 복습

---
+++ java-basic/src/access/a/PublicClass.java
@@ -0,0 +1,17 @@
+package access.a;
+
+public class PublicClass {
+    public static void main(String[] args) {
+        PublicClass publicClass = new PublicClass();
+        DefaultCalss1 class1 = new DefaultCalss1();
+        DefaultCalss2 class2 = new DefaultCalss2();
+    }
+}
+
+class DefaultCalss1 {
+
+}
+
+class DefaultCalss2 {
+
+}
