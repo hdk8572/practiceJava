f19d0cc22ef2df3ac463913a8e2a1f718d1223b2
Whatwhy <hdk8572@gmail.com>
2024-01-15 23:58:49 +0900

    상속 공부

---
+++ java-basic/src/extends1/ex1/ElectricCar.java
@@ -0,0 +1,11 @@
+package extends1.ex1;
+
+public class ElectricCar {
+    public void move() {
+        System.out.println("차를 이동합니다.");
+    }
+    public void charger() {
+        System.out.println("충전합니다.");
+    }
+
+}
