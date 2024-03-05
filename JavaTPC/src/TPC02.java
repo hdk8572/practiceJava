93c47f9c7941b454b288c1df7b72fadeb9df28ab
Whatwhy <hdk8572@gmail.com>
2024-03-05 09:34:44 +0900

    자바 공부

---
+++ JavaTPC/src/TPC02.java
@@ -0,0 +1,8 @@
+
+public class TPC02 {
+	public static void main(String[] args) {
+		// 프로그래밍의 3대요소 : 변수, 자료형(DataType), 할당(=)
+		
+		
+	}
+}
--- hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,6 +1,7 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
@@ -24,7 +25,7 @@ public class MemoryMemberRepository implements MemberRepository {
 
     @Override
     public Optional<Member> findByName(String name) {
-        store.values().stream()
+       return store.values().stream()
                 .filter(member -> member.getName().equals(name))
                 .findAny();
     }
@@ -33,4 +34,9 @@ public class MemoryMemberRepository implements MemberRepository {
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
     }
+
+    public void clearStore() {
+        store.clear();
+    }
+
 }
